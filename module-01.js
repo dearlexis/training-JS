44fad0c1f51ca9a1f4c18d6dff23262f253ef8de
dearlexis <104580408+dearlexis@users.noreply.github.com>
2022-08-09 15:16:16 +0300

    1

---
+++ module-01.js
@@ -0,0 +1,66 @@
+// const message = 'Mango ' + 'is' + ' happy';
+// console.log(message);
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// let password = '';
+
+// do {
+//   password = prompt('Введіть пароль довший 4-х символів', '');
+// } while (password.length < 5);
+
+// console.log('Ввели пароль: ', password);
+
+// const colors = ['red', 'green', 'blue'];
+
+// colors.splice(2, 0, 'purple');
+// console.log(colors); // ["red", "green", "purple", "blue"]
+
+// function add(a, b, c) {
+//   // Change code below this line
+
+//   return a + b + c;
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// function checkStorage(available, ordered) {
+//   const available = 100;
+//   const ordered = 50;
+//   let message;
+//   console.log(message);
+
+//   if (ordered > available) {
+//     message = 'Not enough goods in stock!';
+//   } else {
+//     message = 'Order is processed, our manager will contact you.';
+//   }
+
+//   return message;
+// }
+
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// // Change code below this line
+// const fruitsArrayLength = fruits.length;
+// console.log(fruitsArrayLength);
+
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+
+// // Change code below this line
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// console.log(lastElementIndex);
+// console.log(lastElement);
